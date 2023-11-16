70a54e727ecc7fa2552bbe2972090f00cbd796e2
= <=dialloabdou@hotmail.com>
2023-11-16 21:27:22 +0100

    Gestion XMl en memoire XDocument

---
+++ AppSerialisation/PersonneDocumentXml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AppSerialisation
+{
+    public class PersonneDocumentXml
+    {
+
+        /// <summary>
+        ///  Lecture xDocuments
+        /// </summary>
+        /// <param name="fichier"></param>
+        /// <param name="personne"></param>
+        /// <returns></returns>
+        public static Personne ReadPersonneDocXml(string fichier, Personne personne)
+        {
+
+            var doc = XDocument.Load(fichier);
+
+            if (doc.Root is not null)
+            {
+                foreach (var item in doc.Root.Elements())
+                {
+                    if (item.Name == "Nom")
+                    {
+                        personne.Nom = item.Value;
+                    }
+                    else if (item.Name == "Prenom")
+                    {
+                        personne.Prenom = item.Value;
+                    }else if(item.Name == "DateDeNaissance")
+                    {
+                        personne.DateDeNaissance = DateTime.Parse(item.Value);
+                    }
+                    else
+                    {
+                        personne.Taille = int.Parse(item.Value);
+                    }
+                }
+
+            }
+            return personne;
+
+        }
+
+        public static void AddPersonneDocXml(string fichier)
+        {
+            var doc = XDocument.Load (fichier);
+            var Adresse = new XElement("Adresse", "16 rue Felibre Gaut 13100 Aix-en-provence");
+            if (doc.Root is not null)
+            {
+                var elementTaille = doc.Root.Element("Taille");
+                if(elementTaille is not null)
+                {
+                    elementTaille.AddAfterSelf(Adresse);
+                }
+
+            }
+
+            Console.WriteLine(doc);
+        }
+
+
+    }
+}
--- AppSerialisation/Program.cs
+++ AppSerialisation/Program.cs
@@ -1,13 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Xml;
+using System.Xml.Linq;
 using AppSerialisation;
 
 Personne personne = new Personne();
-Console.WriteLine(" Lecture XML avec XMLReader !");
-// Appelle de la methode  Reader
-PersonneReader.AfficheReadXmlPersonne(personne);
+//Console.WriteLine(" Lecture XML avec XMLReader !");
+//// Appelle de la methode  Reader
+//PersonneReader.AfficheReadXmlPersonne(personne);
+//// Appelle de la methode Writer
+//PersonneWriter.CreateXmlPersonne(personne);
 
-// Appelle de la methode Writer
-PersonneWriter.CreateXmlPersonne(personne);
-//Console.ReadLine();
+// utilisation des documents 
+
+var fichier = @"C:\FormationC#\Serialisation\AppSerialisation\serialisationXml.xml";
+if (fichier is not null)
+{
+    var personneFin = PersonneDocumentXml.ReadPersonneDocXml(fichier, personne);
+
+    Console.WriteLine($"les Données Personne Nom: {personneFin.Nom}, Prenom : {personneFin.Prenom}, DateNaissance : {personneFin.DateDeNaissance:dd/mm/yyyy}, Taille : {personneFin.Taille} cm");
+}
+
+if (fichier is not null)
+PersonneDocumentXml.AddPersonneDocXml(fichier);
+
+
 
